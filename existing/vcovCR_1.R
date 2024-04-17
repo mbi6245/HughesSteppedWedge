6e0e8b31e334bf721d61991f31aa47c62f2743c4
mbi6245 <mbi6245@uw.edu>
2024-04-16 21:23:46 -0700

    prof hughes data and code to extract crucial parts of objects

---
+++ existing/vcovCR_1.R
@@ -0,0 +1,32 @@
+library("sandwich")
+library("clubSandwich")
+library("lme4")
+#############
+ctdata$fcluster = factor(ctdata$hdist)
+ctdata$ftime = factor(ctdata$time)
+ctdata$clustime = interaction(ctdata$fcluster,ctdata$ftime)
+rslt = lmer(ct ~ ftime + treat + (1 | fcluster) + (1 | clustime), data=ctdata)
+diag(vcov(rslt))  # model-based
+diag(rvar0 = vcovCR(rslt, cluster=ctdata$fcluster, type="CR0")) # classic Liang-Zeger
+#####################
+# write function to reproduce classic Liang-Zeger result
+#####################
+# Here are some ways of extracting information from the model object
+beta=matrix(fixef(rslt),ncol=1)
+np=dim(beta)[1]
+gamma = matrix(c(ranef(rslt)$clustime,ranef(rslt)$fcluster),ncol=1)
+nq=dim(gamma)[1]
+X = model.matrix(rslt,type="fixed")
+Z = model.matrix(rslt,type="random")
+Y = rslt@resp$y # example of "slots" in R
+cluster = clubSandwich:::get_outer_group(rslt) # maybe there is a better way to do this
+eta = predict(rslt,type="link")
+ginv_eta = predict(rslt,type="response")
+# Note that delta = Identity matrix for identity link
+# more generally, we can use family(rslt)$link to find the link
+delta = diag(nobs(rslt))
+deltainv = delta # in this case
+theta = as.data.frame(VarCorr(rslt))
+R = diag(c(rep(theta$vcov[1],ngrps(rslt)["clustime"]),rep(theta$vcov[2],ngrps(rslt)["fcluster"])))
+G = ngrps(rslt)["fcluster"]
+
