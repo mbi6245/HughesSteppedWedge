e9eeab711a86cd3d876c6b1f7d85d50deeb9151f
mbi6245 <mbi6245@uw.edu>
2024-05-07 15:18:37 -0700

    initial commit of max's function

---
+++ analysis/vcovCR_glmerMod_Max.R
@@ -0,0 +1,13 @@
+#' Title
+#'
+#' @param obj
+#' @param cluster
+#' @param type
+#'
+#' @return
+#' @export
+#'
+#' @examples
+vcovCR_glmerMod_Max = function(obj, cluster, type="classic") {
+  return(NA)
+}
